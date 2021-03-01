64fc669c64ac30887f146b9489503fae9965295e
Zhixiang Li <li232759@linux1c.csc.uvic.ca>
2021-02-28 19:27:04 -0800

    part 1

--- 04_assign/functions.rkt
+++ 04_assign/functions.rkt
@@ -11,28 +11,73 @@
     (lambda () (f 0))))
 
 ;#1
-(define add-pointwise  #f)
+(define add-pointwise 
+  (lambda (xs ys) 
+    (if (and (list? xs) (list? ys))
+      (if (null? xs)
+          (if (null? ys)
+              null
+              ys)
+          (if (null? ys)
+              xs
+              (cons (+ (car xs) (car ys)) (add-pointwise (cdr xs) (cdr ys)))))
+      (error "illegal parameter"))))
+    
 
 ;#2
-(define add-pointwise-lists #f)
+(define add-pointwise-lists 
+  (lambda (xs) 
+    (if (null? (cdr xs))
+        (car xs)
+        (add-pointwise (car xs) (add-pointwise-lists (cdr xs))))))
 
 ;#3
-(define add-pointwise-lists-2 #f)
+(define add-pointwise-lists-2 
+  #f)
 
 ;#4
-(define stream-for-n-steps #f)
+(define stream-for-n-steps 
+  (lambda (stream n)
+    (if (> n 0)
+      (cons (car (stream)) (stream-for-n-steps (cdr (stream)) (- n 1)))
+      null)))
 
 ;#5
-(define fibo-stream #f)
+(define fibo-stream 
+  (letrec
+      ([f (lambda (x y)
+            (if (= (+ x y) 0)
+            (cons 0 (lambda () (f 1 0)))
+            (cons (+ x y) (lambda () (f y (+ x y))))))])
+      (lambda () (f 0 0))))
 
 ;#6
-(define filter-stream #f)
+(define filter-stream 
+  (letrec
+      ([f (lambda (filter stream)
+            (if (filter (car (stream)))
+            (cons (car (stream)) (lambda () (f filter (cdr (stream)))))
+            (f filter (cdr (stream))) ))])
+      (lambda (x y) (lambda () (f x y)))))
 
 ;#7
-(define palyndromic-numbers #f)
+(define palyndromic-numbers 
+  (letrec 
+      ([pal (lambda (n) 
+          (let ([li (string->list (number->string n))])
+          (equal? li (reverse li)))  )])
+      (filter-stream pal nat-num-stream)))
 
 ;#8 macro create-stream
-
+(define-syntax create-stream 
+  (syntax-rules (using starting at with increment)
+    [(create-stream name using fu starting at i0 with increment delta) 
+     (define name 
+        (letrec
+          ( [func fu]
+            [f (lambda (x)
+                (cons (func x) (lambda () (f (+ x delta)))))])
+        (lambda () (f i0)))   )]))
 ; part 2
 
 ;#1
