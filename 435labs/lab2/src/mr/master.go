5477fa2ad4caa5f3df79dbb07ad83c9c652d7fc4
ZhixiangL <lizhixiang27@gmail.com>
2021-05-22 14:16:47 -0700

    d

---
+++ 435labs/lab2/src/mr/master.go
@@ -0,0 +1,74 @@
+package mr
+
+import "log"
+import "net"
+import "os"
+import "net/rpc"
+import "net/http"
+
+
+type Master struct {
+	// Your definitions here.
+	pointer int
+	files string[]
+	nReduce int
+}
+
+// Your code here -- RPC handlers for the worker to call.
+
+//
+// an example RPC handler.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+func (m *Master) Example(args *Args, reply *Reply) error {
+	reply.Y = args.X + 1
+	return nil
+}
+
+
+//
+// start a thread that listens for RPCs from worker.go
+//
+func (m *Master) server() {
+	rpc.Register(m)
+	rpc.HandleHTTP()
+	//l, e := net.Listen("tcp", ":1234")
+	sockname := masterSock()
+	os.Remove(sockname)
+	l, e := net.Listen("unix", sockname)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+}
+
+//
+// main/mrmaster.go calls Done() periodically to find out
+// if the entire job has finished.
+//
+func (m *Master) Done() bool {
+	ret := false
+
+	// Your code here.
+
+
+	return ret
+}
+
+//
+// create a Master.
+// main/mrmaster.go calls this function.
+// nReduce is the number of reduce tasks to use.
+//
+func MakeMaster(files []string, nReduce int) *Master {
+	m := Master{}
+
+	// Your code here.
+	m.files = files
+	m.pointer = 0
+	m.nReduce = nReduce
+
+	m.server()
+	return &m
+}
