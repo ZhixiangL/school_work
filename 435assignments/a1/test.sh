4650985c14a5d15b950d6f227b813cf8510010d7
Zhixiang Li <li232759@linux2c.csc.uvic.ca>
2021-06-03 11:29:29 -0700

    435a1 done

---
+++ 435assignments/a1/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+make
+echo "\n>>>>>>>>>>Valid tests and diff start<<<<<<<<<<"
+java Compiler tests/valid_guideline.ul | diff - tests/valid_guideline.ul
+java Compiler tests/valid_func_decl.ul | diff - tests/valid_func_decl.ul
+java Compiler tests/valid_expression.ul | diff - tests/valid_expression.ul
+java Compiler tests/valid_statement.ul | diff - tests/valid_statement.ul
+java Compiler tests/accept_array_outofbound.ul | diff - tests/accept_array_outofbound.ul
+java Compiler tests/accept_expression.ul | diff - tests/accept_expression.ul
+java Compiler tests/accept_function_parameter.ul | diff - tests/accept_function_parameter.ul
+java Compiler tests/accept_variable.ul | diff - tests/accept_variable.ul
+java Compiler tests/accept_incorrect_functioncall.ul | diff - tests/accept_incorrect_functioncall.ul
+echo "\n>>>>>>>>>>Invalid tests start<<<<<<<<<<"
+index=1
+while [ $index -lt 15 ]
+do
+    java Compiler tests/invalid$index.ul
+    index=$(( $index + 1 ))
+done
+
+make clean
