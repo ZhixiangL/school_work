f272922ceda9190410c257605aff2b744ace8d24
ZhixiangL <lizhixiang27@gmail.com>
2021-07-18 19:02:06 -0700

    delete old files

--- 435assignments/a1/AST/Statement.java
+++
@@ -1,7 +0,0 @@
-package AST;
-import Type.*;
-
-public abstract class Statement extends ASTNode {
-    public abstract void accept (Visitor v);
-    public abstract Type accept (TypeVisitor v);
-}
--- 435assignments/a1/Type/Type.java
+++
@@ -1,7 +0,0 @@
-package Type;
-
-public abstract class Type {
-    public abstract String toString();
-
-    public abstract boolean equals(Object o);
-}
